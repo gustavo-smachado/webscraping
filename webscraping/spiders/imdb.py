6d0ce368015a2edfadbcf33a1a8e13f6b87a2a45
Gustavo da Silva Machado <gugasilva.machado@gmail.com>
2022-07-04 23:03:29 -0300

    Initial commit

---
+++ webscraping/spiders/imdb.py
@@ -0,0 +1,14 @@
+import scrapy
+
+
+class ImdbSpider(scrapy.Spider):
+    name = 'imdb'    
+    start_urls = ['https://www.imdb.com/chart/top/?ref_=nv_mv_250']
+
+    def parse(self, response):
+        for filmes in response.css('.titleColumn'):
+            yield{
+                'titulos': filmes.css('.titleColumn a::text').get(),
+                'anos': filmes.css('.secondaryInfo::text').get()[1:-1],
+                'nota': response.css('strong::text').get()
+                }
